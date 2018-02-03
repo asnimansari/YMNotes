d15f9b316b2d42a5fe865c2907bfe75668dc1e4e
Asnim P Ansari <asnim96@gmail.com>
2018-02-03 10:07:35 +0530

    MainActivity->YM

--- app/src/main/java/com/yesmeal/yesmealnotes/Home.kt
+++ app/src/main/java/com/yesmeal/yesmealnotes/Home.kt
@@ -5,7 +5,7 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
