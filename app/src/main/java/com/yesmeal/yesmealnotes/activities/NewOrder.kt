86f11e0a653ee74fb06fa7584d2dc74c56c19478
Asnim P Ansari <asnim96@gmail.com>
2018-02-03 12:26:34 +0530

    Hiding-> Changing Focous

--- app/src/main/java/com/yesmeal/yesmealnotes/activities/NewOrder.kt
+++ app/src/main/java/com/yesmeal/yesmealnotes/activities/NewOrder.kt
@@ -12,19 +12,31 @@ class NewOrder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_order)
+        updateOrderForm(false)
+
 
         toggleButton.setOnCheckedChangeListener(object :CompoundButton.OnCheckedChangeListener{
             override fun onCheckedChanged(p0: CompoundButton?, isChecked: Boolean) {
                 updateOrderForm(isChecked)
-
             }
-
         })
+        saveButton.setOnClickListener {
+            var shopName = shopName.text.toString().trim()
+            var location = location.text.toString().trim()
+            var landmark = landmark.text.toString().trim()
+            var mobileNumber = mobileNumber.text.toString().trim()
+            var serviceChargePayedLater  = serviceChargePayedLater.isChecked
+            var collectServiceChargeFromShop  = collectServiceChargeFromShop.isChecked
+
+
+        }
     }
 
 
     fun updateOrderForm(isYesMealOrder:Boolean){
-        landmark.visibility = if(isYesMealOrder) View.VISIBLE else View.GONE
-        mobileNumber.visibility = landmark.visibility
+        landmark.isFocusable = isYesMealOrder
+        landmark.isClickable =  isYesMealOrder
+        mobileNumber.isFocusable = isYesMealOrder
+        mobileNumber.isClickable = isYesMealOrder
     }
 }
